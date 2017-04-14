a113b3f182ebc86e29ce5d6e13a391575f7b4bec
JK <kabiljayaraman@gmail.com>
2017-04-14 08:52:18 +0400

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,4 +1,4 @@
-var custConfig = require('./custom.js');
+var dynconfig = require('custom-config');
 console.log('Item : ' + custConfig.Item('SampleData/pagesize'));
 console.log('Items : ' + custConfig.Items('SampleData/servers'));
 
